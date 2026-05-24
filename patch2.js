18e38bc08369e7cfdbc2f4e714c9bf80dad57871
JongBatak <zwinglisav@gmail.com>
2026-05-24 14:33:37 +0700

    removing unused files from repo

--- patch2.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'Frontend/src/components/landing/VelocityMarquee.tsx';
-let txt = fs.readFileSync(file, 'utf8');
-
-// The replacement logic is getting complex, I'll just write to the user a complete refactored file content directly to patch it in.
